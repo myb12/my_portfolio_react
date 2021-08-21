e6f4fe136b178b128a101a21ad237fa194057d42
myb12 <mohammadyasinbappy@outlook.com>
2021-08-22 01:54:42 +0600

    deployment error fixed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,3 @@
-import React, { isValidElement } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
